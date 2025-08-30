046a08e175dc22d025b46610b03ce8983fc4d84e
oleks <oleksandr.klochenko@gmail.com>
2025-08-31 00:47:40 +0300

    30-08-2025 Configure project architecture

--- src/main/java/amfgProject/Common/WaitHelper.java
+++ src/main/java/amfgProject/Common/WaitHelper.java
@@ -1,4 +1,32 @@
 package amfgProject.Common;
 
-public class WaitHelper {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class WaitHelper
+{
+
+    /**
+     * Wait methods for visibility Of Element Located
+     */
+    public static WebElement WaitVisibilityOfElementLocated (WebDriver driver, By loctor, int time)
+    {
+        return (new WebDriverWait(driver, Duration.ofSeconds(time))).until(ExpectedConditions.visibilityOfElementLocated(loctor));
+    }
+
+
+    /**
+     * Wait methods for invisibility of Element presented on the page
+     */
+    public static boolean WaitForInvisibilityOfElementLocated2(WebDriver driver, By loctor, int time)
+    {
+        return (new WebDriverWait(driver, Duration.ofSeconds(time))).until(ExpectedConditions.invisibilityOfElementLocated(loctor));
+    }
+
+
 }
--- src/test/java/amfgProject/Base/BaseTest.java
+++ src/test/java/amfgProject/Base/BaseTest.java
@@ -1,6 +1,7 @@
 package amfgProject.Base;
 
 import amfgProject.Common.DriverUtils;
+import amfgProject.Pages.AmfgFE.*;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.platform.suite.api.Select;
@@ -14,6 +15,15 @@ import static amfgProject.Common.Config.NEED_TO_CLOSE_BROWSER_AFTER_TEST;
 public class BaseTest
 {
     WebDriver driver= DriverUtils.createDriver();
+
+    AuthFePage authFePage =new AuthFePage(driver);
+    CatalogueFePage catalogueFePage=new CatalogueFePage(driver);
+    DashboardFePage dashboardFePage=new DashboardFePage(driver);
+    InstantQuoteFePage instantQuoteFePage=new InstantQuoteFePage(driver);
+    MyListFePage myListFePage=new MyListFePage(driver);
+    ProjectsFePage projectsFePage=new ProjectsFePage(driver);
+    QuotesFePage quotesFePage=new QuotesFePage(driver);
+
 
 
     @AfterAll
